efec0c5f805a2a109efcad4cd4549d091926a23f
ShaAlreadyHeer <Shaheerahmed23@gmail.com>
2022-08-17 21:39:35 -0400

    Added Description View, and rearranged HomeScreen Views

--- Furniture_app/ContentView.swift
+++ Furniture_app/ContentView.swift
@@ -11,10 +11,22 @@ struct ContentView: View {
 
     var body: some View {
         VStack {
-            HomeScreen()
-        }
+            ZStack{
+                Color(.lightGray)
+                    .edgesIgnoringSafeArea(.all)
+            
+            VStack{
+                Image("ChaatMasala")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .edgesIgnoringSafeArea(.top)
+            
+                DescriptionView()
+            }
         }
     }
+    }
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
@@ -23,201 +35,30 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
-struct AppBarView: View {
-    var body: some View {
-        HStack{
-            Button(action: {}){
-                Image("menu")
-                    .padding()
-                    .background(.white)
-                    .cornerRadius(10.0)
-            }
-            Spacer()
-            Button(action:{}){
-                Image(uiImage: #imageLiteral(resourceName: "ShaheerProfile"))
-                    .resizable()
-                    .frame(width:42, height:42)
-                    .cornerRadius(10.0)
-            }
-        }
-        .padding(.horizontal)
-    }
-}
 
-struct TagLineView: View {
-    var body: some View {
-        Text("Find The \nBest ")
-            .font(.custom("PlayfairDisplay-Regular",size:28))
-            .foregroundColor(Color("Primary"))
-        + Text("Deals!")
-            .font(.custom("PlayfairDisplay-Bold",size:28))
-            .foregroundColor(Color("Primary"))
-    }
-}
 
-struct SearchBarView: View {
-    @State private var search: String = ""
+struct DescriptionView: View {
     var body: some View {
-        HStack{
-            HStack{
-                Image("Search")
-                    .padding(.trailing,8)
-                TextField("Search Deals",text:$search)
-            }
-            
-            .padding(.all,20)
-            .background(Color.white)
-            .cornerRadius(10.0)
-            .padding(.trailing)
-            
-            Button(action:{}) {
-                Image("Scan")
-                    .resizable()
-                    .frame(width: 35, height: 30)
-                    .padding()
-                    .background(Color("Primary"))
-                    .cornerRadius(10.0)
-            }
-        }
-    }
-}
-
-struct CategoryView: View {
-    let isActive: Bool
-    let text: String
-    var body: some View {
-        HStack{
-            VStack(alignment: .leading, spacing: 0){
-                Text(text)
-                    .font(.system(size: 18))
-                    .fontWeight(.medium)
-                    .foregroundColor(isActive ? Color("Primary"): Color.black.opacity(0.5))
-                
-                if(isActive){
-                Color("Primary")
-                    .frame(width: 15, height: 2)
-                    .clipShape(Capsule())
-                }
-            }
-            .padding(.trailing)
-        }
-    }
-}
-
-struct ProductCardView: View {
-    let image:Image
-    let size:CGFloat
-    var body: some View {
-        VStack {
-            Image("ChaatMasala")
-                .resizable()
-                .frame(width: size, height: 200 * (size/210))
-                .cornerRadius(20.0)
-            
+        VStack (alignment: .leading){
             Text("Shan Chaat Masala")
-                .font(.title3)
+                .font(.title)
                 .fontWeight(.bold)
-            HStack (spacing: 2){
-                ForEach(0 ..< 5){item in
+            
+            HStack (spacing: 4){
+                ForEach(0 ..< 5){ item in
                     Image("star")
                 }
+                
+                Text("4.9")
+                    .opacity(0.5)
+                    .padding(.leading,8)
                 Spacer()
-                
-                Text("$2.99")
-                    .font(.title3)
-                    .fontWeight(.bold)
-                
-            }
-        }
-        .frame(width:size)
-        .padding()
-        .background(Color.white)
-        .cornerRadius(20.0)
-    }
-}
-
-struct BottomNavBarItem: View {
-    let image: Image
-    let action: ()-> Void
-    var body: some View {
-        Button(action: action,label:{
-            image.frame(maxWidth:.infinity)
-        }
-        )
-    }
-}
-
-struct HomeScreen: View {
-    private let categories = ["All","Spices","Dairy","Naan","Fruit & Veggies","Placeholder","Extras"]
-    @State private var selectedIndex: Int = 0
-    var body: some View {
-        ZStack{
-            Color(.lightGray)
-                .edgesIgnoringSafeArea(.all)
-            
-            //Popular and New Stock
-            ScrollView {
-                VStack (alignment: .leading){
-                    AppBarView()
-                    TagLineView()
-                        .padding()
-                    SearchBarView()
-                    
-                    ScrollView(.horizontal, showsIndicators: false) {
-                        HStack{
-                            ForEach(0 ..< categories.count) { i in
-                                //If Index == 1 then isActive is true
-                                CategoryView(isActive: i == selectedIndex, text: categories[i])
-                                    .onTapGesture{selectedIndex = i}
-                            }
-                        }
-                        .padding()
-                    }
-                    
-                    Text("Popular")
-                        .font(.custom("PlayfairDisplay-bold",size: 24))
-                        .padding(.horizontal)
-                    
-                    ScrollView(.horizontal,showsIndicators: false){
-                        HStack{
-                            ForEach(0 ..< 4){index in
-                                ProductCardView(image:Image("ChaatMasala_\(index + 1)"),size: 210)
-                            }
-                            .padding(.trailing)
-                        }
-                        .padding(.leading)
-                    }
-                    
-                    Text("Popular")
-                        .font(.custom("PlayfairDisplay-bold",size: 24))
-                        .padding(.horizontal)
-                    
-                    ScrollView(.horizontal,showsIndicators: false){
-                        HStack{
-                            ForEach(0 ..< 4){index in
-                                ProductCardView(image:Image("ChaatMasala_\(index + 1)"),size: 180)
-                            }
-                            .padding(.trailing)
-                        }
-                        .padding(.leading)
-                    }
-                }
             }
             
-            //Custom Navigation Bar
-            
-            HStack{
-                BottomNavBarItem(image:Image("Home")){}
-                BottomNavBarItem(image:Image("Heart")){}
-                BottomNavBarItem(image:Image("Shop")){}
-                BottomNavBarItem(image:Image("User")){}
-            }
             .padding()
+            .padding(.top)
             .background(Color.white)
-            .clipShape(Capsule())
-            .padding(.horizontal)
-            .shadow(color: Color.black.opacity(0.50), radius: 8, x: 2, y: 6)
-            .frame(maxHeight:.infinity,alignment: .bottom)
+            .cornerRadius(40.0)
         }
     }
 }
